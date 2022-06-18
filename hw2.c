8fc20b41821e785d5d88c58e95113125581d38b2
darkdulgi <daniel99285@gmail.com>
2022-06-18 14:25:43 +0900

    6/18

--- hw2.c
+++ hw2.c
@@ -28,25 +28,30 @@ int RemoveFile(char *name)
 {
 }
 
-int MakeDirectory(char *name) // 상위 디렉토리가 존재하지 않으면 -1, 성공적으로 생성 시 0 반환
-{
+int MakeDirectory(char *name) // 실패시 -1, 성공적으로 생성 시 0 반환
+{   
+    char tname[30];     // 경로의 깊이 계산
+    strcpy(tname, name);
+    char *temp = strtok(tname, "/");
+    int depth = -1;
+    while(temp != NULL){
+        temp = strtok(NULL, "/");
+        depth++;
+    }
+
     int newinodenum = GetFreeInodeNum();
     int newblocknum = GetFreeBlockNum();
-    
     Inode *pinode = malloc(sizeof(Inode));
     DirEntry *pdirentry = malloc(sizeof(DirEntry));
     GetInode(0, pinode);
-    char *nameread = strtok(name, "/");
 
-    
+    int here = 0;
+    char *nameread = strtok(name, "/");
     while (nameread != NULL)
     {
-        printf("%s ",nameread);
         int curblocknum = pinode->dirBlockPtr[0];
-        char *temp = nameread;
-        int isEND = 0;
-        /*
-        if (isEND == 0)
+        
+        if (here < depth)
         {
             int dirFind = 0;
             for (int i = 0; i < 16; i++) {
@@ -63,7 +68,9 @@ int MakeDirectory(char *name) // 상위 디렉토리가 존재하지 않으면 -
             GetInode(pdirentry->inodeNum, pinode);
         }
         else {
-            for (int i = 0; i < 16; i++) {
+            GetDirEntry(curblocknum, 0, pdirentry)
+            int parentinodenum = pdirentry->inodeNum;
+            for (int i = 1; i < 16; i++) {
                 if (GetDirEntry(curblocknum, i, pdirentry) == INVALID_ENTRY){  // 비어있는 엔트리에서 새 다랙토리 생성
                     strcpy(pdirentry->name, nameread);
                     pdirentry->inodeNum = newinodenum;
@@ -82,19 +89,24 @@ int MakeDirectory(char *name) // 상위 디렉토리가 존재하지 않으면 -
                     DirEntry *ptr = malloc(BLOCK_SIZE);
                     strcpy(ptr[0].name, ".");
                     ptr[0].inodeNum = newinodenum;
-                    for (int i = 1; i < 16; i++)
+                    strcpy(ptr[1].name, "..");
+                    ptr[1].inodeNum = parentinodenum;
+                    for (int i = 2; i < 16; i++)
                        ptr[i].inodeNum = INVALID_ENTRY;
                     DevWriteBlock(newblocknum, ptr);
                     free(ptr);
                     break;
+                }
+                else if(strcmp(pdirentry->name, nameread) == 0){    // 같은 이름의 디렉토리가 존재함
+                    return -1;
                 }
                 if(i == 15) {    // 디렉토리가 꽉 차있음
                     return -1; 
                 }
             }
         }
-        */
         
+        here++;
         nameread = strtok(NULL, "/");
     }
     
@@ -110,8 +122,69 @@ int MakeDirectory(char *name) // 상위 디렉토리가 존재하지 않으면 -
     return 0;
 }
 
-int RemoveDirectory(char *name)
+int RemoveDirectory(char *name) // 실패시 -1, 성공적으로 제거 시 0 반환
 {
+    char tname[30];     // 경로의 깊이 계산
+    strcpy(tname, name);
+    char *temp = strtok(tname, "/");
+    int depth = -1;
+    while(temp != NULL){
+        temp = strtok(NULL, "/");
+        depth++;
+    }
+
+    Inode *pinode = malloc(sizeof(Inode));
+    DirEntry *pdirentry = malloc(sizeof(DirEntry));
+    GetInode(0, pinode);
+
+    int here = 0;
+    char *nameread = strtok(name, "/");
+    while (nameread != NULL)
+    {
+        int curblocknum = pinode->dirBlockPtr[0];
+        
+        if (here < depth)
+        {
+            int dirFind = 0;
+            for (int i = 0; i < 16; i++) {
+                if (GetDirEntry(curblocknum, i, pdirentry) == INVALID_ENTRY) continue;
+                if (strcmp(pdirentry->name, nameread) == 0){
+                    dirFind = 1;
+                    break;
+                }
+            }
+
+            if(dirFind == 0){   // 상위 디렉토리가 존재하지 않음
+                return -1;
+            }
+            GetInode(pdirentry->inodeNum, pinode);
+        }
+        else {
+            GetDirEntry(curblocknum, 0, pdirentry)
+            int parentinodenum = pdirentry->inodeNum;
+            for (int i = 1; i < 16; i++) {
+                
+                
+                if(i == 15) {    
+                    return -1; 
+                }
+            }
+        }
+        
+        here++;
+        nameread = strtok(NULL, "/");
+    }
+    
+    
+    FileSysInfo *info = malloc(BLOCK_SIZE); // 디렉토리 생성을 마친 후 FileInfo 수정
+    DevReadBlock(0, info);
+    info->numAllocBlocks--;
+    info->numFreeBlocks++;
+    info->numAllocInodes--;
+    DevWriteBlock(0, info);
+    free(info);
+    
+    return 0;
 }
 
 void CreateFileSystem(void)
